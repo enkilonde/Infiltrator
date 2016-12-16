c0159501ca6c5047273fb74d450cd302c014b8ff
Enki <enkilonde@gmail.com>
2016-12-16 12:35:26 +0100

    Rangement projet + ajout shader forge

---
+++ Assets/PROJECT/Scripts/Procedural/ProceduralValues.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProceduralValues
+{
+
+    public static float timeElapsed; 
+    //NEWVAR - Enki - +comment
+    //End NEWVAR
+
+
+
+
+
+    public static void ResetValues()
+    {
+        timeElapsed = 0;
+
+
+    }
+}
