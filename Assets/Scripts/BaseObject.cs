8e6d515743874aadd87cf4765526278084cbc6b3
Enki <enkilonde@gmail.com>
2016-12-16 11:52:05 +0100

    Initial Commit

---
+++ Assets/Scripts/BaseObject.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseObject : MonoBehaviour
+{
+
+    bool loadingEnded = false;
+
+    void Awake()
+    {
+        StartCoroutine(StartingProcess());
+    }
+    
+
+    IEnumerator StartingProcess()
+    {
+
+        FirstAwake();
+
+        yield return null;
+
+        SecondAwake();
+
+        yield return null;
+
+        OnLoadEnded();
+
+    }
+
+    //C'est ici qu'on attribue les références
+    protected virtual void FirstAwake()
+    {
+
+    }
+
+    //on utilisera cette fonction comme on utilise l'Awake normalement.
+    protected virtual void SecondAwake()
+    {
+
+    }
+
+    
+    protected virtual void OnLoadEnded()
+    {
+        loadingEnded = true;
+    }
+
+
+    void Update()
+    {
+        if (loadingEnded) BaseUpdate();
+    }
+
+    protected virtual void BaseUpdate()
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : BaseObject
+{
+    private Rigidbody playerRigidbody;
+
+    public float playerSpeed = 5;
+
+    public GameObject test;
+
+    protected override void FirstAwake()
+    {
+        base.FirstAwake();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+
+    protected override void BaseUpdate()
+    {
+        base.BaseUpdate();
+
+        MovePlayer();
+
+        PlayerLookAt();
+
+    }
+
+    void MovePlayer()
+    {
+
+        Vector3 direction = Vector3.zero;
+
+        direction.x = Input.GetAxisRaw("Horizontal");
+        
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        playerRigidbody.velocity = direction.normalized * playerSpeed;
+    }
+
+    void PlayerLookAt()
+    {
+        var mousePos2D = Input.mousePosition;
+        mousePos2D.z = Camera.main.transform.position.y;
+        mousePos2D = Camera.main.ScreenToWorldPoint(mousePos2D);
+        transform.LookAt(new Vector3(mousePos2D.x, transform.position.y, mousePos2D.z));
+
+    }
+
+}
