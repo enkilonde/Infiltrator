3e3ef77714158f40d3579337e0404a39bc1dd623
Arckantox <arckantox77@hotmail.fr>
2017-01-16 01:35:16 +0100

    Ennemis "Soldier" prefab avec pattern modulable via les valeurs public (désactiver guizmo "canvas" pour enlever les carrés blancs)

---
+++ Assets/PROJECT/Scripts/EnemyScript/Soldier.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Soldier : BaseEnemy
+{
+    public int patternSelect;
+    public int sizeX, sizeY;
+
+    public float viewA, viewD, spd;
+    public float atkR;
+
+    protected override void FirstAwake()
+    {
+        base.FirstAwake();
+        Instantiated(State.AWAKE, 100f, 100f, viewA, viewD, spd, atkR);
+        //DefinePattern(1, transform.position, 5, 8, 0);
+        DefinePattern(patternSelect, transform.position, sizeX, sizeY, 0);
+    }
+}
